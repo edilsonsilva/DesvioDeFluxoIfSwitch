0c0a59ea8071d2e0e6970bdaef8a53791412a50b
Edilson <edilson_js@yahoo.com.br>
2018-04-19 11:11:33 -0300

    Declaração de Variáveis

--- OperacoesCondicionais/Program.cs
+++ OperacoesCondicionais/Program.cs
@@ -6,7 +6,18 @@ namespace OperacoesCondicionais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Comentário in-line(em linha)
+            /*
+                Comentário in-block(em bloco)
+            */
+            //declaração da variável operação. Ela será do tipo inteira
+            int operacao;
+
+            //declaração das variáveis n1 e n2. Elas serão do tipo double
+            double n1, n2;
+
+
+
         }
     }
 }
